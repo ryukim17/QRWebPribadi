940c7745df3e1cb944f30f50ecee66039a147b82
ryukim17 <mqusa345@gmail.com>
2026-01-29 22:19:45 +0700

    Delete WebIzz directory

--- WebIzz/script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("infoBtn").addEventListener("click", () => {
-  alert(
-    "QR ini berfungsi sebagai identitas kepemilikan barang.\n" +
-    "Data kunjungan dicatat untuk keamanan."
-  );
-});
-
-/* Placeholder tracking (nanti kita sambung ke Supabase) */
-console.log("QR scanned at:", new Date().toISOString());
